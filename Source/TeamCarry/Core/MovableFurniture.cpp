93b4889e7b1a939cc4b994e446e5101de36e8376
이경환\lkh12 <lkh12251011@gmail.com>
2026-06-23 17:28:36 +0900

    [Fix] 인터페이스 CPP파일 삭제

--- Source/TeamCarry/Core/MovableFurniture.cpp
+++
@@ -1,6 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Core/MovableFurniture.h"
-
-// Add default functionality here for any IMovableFurniture functions that are not pure virtual.
--- Source/TeamCarry/Core/StageClearProvider.cpp
+++
@@ -1,6 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "StageClearProvider.h"
-
-// Add default functionality here for any IStageClearProvider functions that are not pure virtual.
