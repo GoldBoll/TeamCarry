099558331c44c2a4976a4c480a9a318111af551c
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-06-23 21:42:55 +0900

    feat(ui): 게임 타이틀 화면 UI, 설정 UI 프로토타입 추가.

---
+++ Source/TeamCarry/UI/SettingWidget.cpp
@@ -0,0 +1,74 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TeamCarry/UI/SettingWidget.h"
+
+#include "Components/Button.h"
+#include "Components/CheckBox.h"
+#include "Components/ComboBoxString.h"
+#include "Components/Slider.h"
+#include "GameFramework/GameUserSettings.h"
+#include "Blueprint/UserWidget.h"
+#include "Input/Events.h"
+#include "InputCoreTypes.h"
+
+void USettingWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	SetIsFocusable(true);
+
+	if (ApplyButton)
+	{
+		ApplyButton->OnClicked.AddUniqueDynamic(this, &USettingWidget::ApplySettings);
+	}
+	if (BackButton)
+	{
+		BackButton->OnClicked.AddUniqueDynamic(this, &USettingWidget::CloseSettings);
+	}
+	if (MasterVolumeSlider)
+	{
+		MasterVolumeSlider->OnValueChanged.AddUniqueDynamic(this, &USettingWidget::HandleMasterVolumeChanged);
+	}
+}
+
+FReply USettingWidget::NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent)
+{
+	if (InKeyEvent.GetKey() == EKeys::Escape)
+	{
+		CloseSettings();
+		return FReply::Handled();
+	}
+
+	return Super::NativeOnKeyDown(InGeometry, InKeyEvent);
+}
+
+void USettingWidget::ApplySettings()
+{
+	if (UGameUserSettings* UserSettings = GEngine ? GEngine->GetGameUserSettings() : nullptr)
+	{
+		if (GraphicsQualityComboBox)
+		{
+			const int32 Quality = FMath::Clamp(GraphicsQualityComboBox->GetSelectedIndex(), 0, 4);
+			UserSettings->SetOverallScalabilityLevel(Quality);
+		}
+
+		if (FullscreenCheckBox)
+		{
+			UserSettings->SetFullscreenMode(FullscreenCheckBox->IsChecked() ? EWindowMode::Fullscreen : EWindowMode::Windowed);
+		}
+
+		UserSettings->ApplySettings(false);
+		UserSettings->SaveSettings();
+	}
+}
+
+void USettingWidget::CloseSettings()
+{
+	OnClosed.Broadcast();
+	RemoveFromParent();
+}
+
+void USettingWidget::HandleMasterVolumeChanged(float Value)
+{
+	OnMasterVolumeChanged.Broadcast(Value);
+}
---
+++ Source/TeamCarry/UI/TitleWidget.h
@@ -0,0 +1,101 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TitleWidget.generated.h"
+
+class UButton;
+class UPanelWidget;
+class UUserWidget;
+
+UCLASS()
+class TEAMCARRY_API UTitleWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI|Title")
+	TSubclassOf<UUserWidget> CharacterSelectWidgetClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI|Title")
+	TSubclassOf<UUserWidget> SettingWidgetClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI|Title")
+	TSubclassOf<UUserWidget> CreditsWidgetClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI|Title")
+	TSubclassOf<UUserWidget> ConfirmPopupWidgetClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI|Title")
+	FName SaveSlotName = TEXT("ManualSave");
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI|Title")
+	int32 SaveUserIndex = 0;
+
+protected:
+	virtual void NativeConstruct() override;
+
+	// Main Menu Buttons
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "UI|Widget")
+	TObjectPtr<UButton> GameStartButton;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "UI|Widget")
+	TObjectPtr<UButton> OptionsButton;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "UI|Widget")
+	TObjectPtr<UButton> CreditsButton;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "UI|Widget")
+	TObjectPtr<UButton> ExitButton;
+
+	// Sub Menu (Continue / New Game)
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "UI|Widget")
+	TObjectPtr<UPanelWidget> SubMenuPanel;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "UI|Widget")
+	TObjectPtr<UButton> ContinueButton;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "UI|Widget")
+	TObjectPtr<UButton> NewGameButton;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "UI|Widget")
+	TObjectPtr<UButton> BackButton;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "UI|Widget")
+	TObjectPtr<UPanelWidget> PopupRoot;
+
+private:
+	UFUNCTION()
+	void HandleGameStartClicked();
+
+	UFUNCTION()
+	void HandleContinueClicked();
+
+	UFUNCTION()
+	void HandleNewGameClicked();
+
+	UFUNCTION()
+	void HandleOptionsClicked();
+
+	UFUNCTION()
+	void HandleCreditsClicked();
+
+	UFUNCTION()
+	void HandleExitClicked();
+
+	UFUNCTION()
+	void HandleBackClicked();
+
+	UFUNCTION()
+	void HandleNewGameConfirm();
+
+	UFUNCTION()
+	void HandleExitConfirm();
+
+	void ShowConfirmPopup(const FText& Message, const FScriptDelegate& OnConfirm);
+
+	bool CheckSaveGame() const;
+	void OpenCharacterSelect();
+};
