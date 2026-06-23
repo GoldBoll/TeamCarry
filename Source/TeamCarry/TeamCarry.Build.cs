ad2c495c9467e5bf9ca032ae1813550b763621b6
이경환\lkh12 <lkh12251011@gmail.com>
2026-06-23 17:51:57 +0900

    [Fix] 빌드 CS 수정

--- Source/TeamCarry/TeamCarry.Build.cs
+++ Source/TeamCarry/TeamCarry.Build.cs
@@ -14,7 +14,7 @@ public class TeamCarry : ModuleRules
 
         PublicIncludePaths.AddRange(new string[] 
         {
-            "TeamCarry",
+            
         });
 
         // Uncomment if you are using Slate UI
